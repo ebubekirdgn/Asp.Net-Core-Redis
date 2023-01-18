aa91df1f8852d5e1f972097317d87e417c916f1e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-01-18 21:01:35 +0300

    jsonserializerobject

--- RedisInMemory/IDistrubutedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ RedisInMemory/IDistrubutedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistrubutedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistrubutedCacheRedisApp.Web.Controllers
 {
@@ -16,16 +18,25 @@ namespace IDistrubutedCacheRedisApp.Web.Controllers
         {
             DistributedCacheEntryOptions distributedCacheEntryOptions = new();
             distributedCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _distrubutedCache.SetString("name","Ebubekir",distributedCacheEntryOptions);
-            await _distrubutedCache.SetStringAsync("surname","dogan",distributedCacheEntryOptions);
+            Product product = new Product {Id =1,Name ="Kalem",Price = 1200 };
+            
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            await _distrubutedCache.SetStringAsync("product:1",jsonProduct,distributedCacheEntryOptions);
+            //_distrubutedCache.SetString("name","Ebubekir",distributedCacheEntryOptions);
+            //await _distrubutedCache.SetStringAsync("surname","dogan",distributedCacheEntryOptions);
             return View();
         }
         public IActionResult Show()
         {
-            string name = _distrubutedCache.GetString("name");
-            string surname = _distrubutedCache.GetString("surname");
-            ViewBag.Name = name;
-            ViewBag.Surname = surname;
+            //string name = _distrubutedCache.GetString("name");
+            //string surname = _distrubutedCache.GetString("surname");
+            //ViewBag.Name = name;
+            //ViewBag.Surname = surname;
+
+            string jsonProduct = _distrubutedCache.GetString("product:1");
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.ProductName = p;  
             return View();  
         }
 
